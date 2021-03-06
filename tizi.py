013f67cd72c1db3f0de35d086830089872ec63f5
U188 <54225629+U188@users.noreply.github.com>
2021-03-06 21:12:27 +0800

    Update tizi.py

--- tizi.py
+++ tizi.py
@@ -1,4 +1,4 @@
-import requests, json, random, re, time,selenium
+import requests, json, random, re, time,selenium,os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
